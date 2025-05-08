60f233fc683c32a27770df8ec9909442b170664b
Kaden Wong <kdw2x@icloud.com>
2025-05-08 10:06:45 -0700

    Feat: Shuffle function guaranteed solvability

--- python/15.py
+++ python/15.py
@@ -1,3 +1,5 @@
+import random
+
 puzzle = [
     ["1", "2", "3", "4"],
     ["5", "6", "7", "8"],
@@ -13,14 +15,36 @@ def checkValidity(validOptions, prompt = ""):
 
     return userInput
 
-def displayPuzzle():
+def checkPuzzleValidity(puzzle):
+    def getInversions(puzzle):
+        inversions = 0
+        flatPuzzle = [int(item, 16) for sublist in puzzle for item in sublist]
+        flatPuzzle.remove(0)
+
+        for i in range(len(flatPuzzle) - 1):
+            if flatPuzzle[i] > flatPuzzle[i + 1]:
+                inversions += 1
+        return inversions
+
+    inversions = getInversions(puzzle) # Number of times a tile is larger than the one after it in a 1D list
+
+    zeroRow = findTile("0")[0] # Row that the blank tile is in
+
+    if (inversions + zeroRow) % 2 == 0: # If the number of inversions added to the numbered row zero is in is even it is solvable
+        return True
+    else:
+        return False
+
+def displayPuzzle(puzzle):
     for row in range(len(puzzle)):
         for val in puzzle[row]:
             print(val, end = " ")
         print()
 
 def findVal(y, x):
-    return puzzle[y][x]
+    value = puzzle[y][x]
+
+    return value
 
 def setTile(y, x, val = "0"): # Defaults to 0
     puzzle[y][x] = val
@@ -60,14 +84,31 @@ def moveTile(move):
 
     print("NOOOO")
 
+def scramblePuzzle(puzzle):
+    firstShuffle = True
+    while not checkPuzzleValidity(puzzle) or firstShuffle:
+        firstShuffle = False
 
-validMoves = ["left", "right", "up", "down", "exit"]
+        for row in puzzle:
+            for col in puzzle:
+                random.shuffle(col)
+        random.shuffle(puzzle)
 
-userInput = ""
-while True:
-    userInput = checkValidity(validMoves, "Enter a valid move")
-    if userInput == "exit":
-        break
-    moveTile(userInput)
-    print()
-    displayPuzzle()
+
+
+
+
+scramblePuzzle(puzzle)
+displayPuzzle(puzzle)
+
+### Test ###
+# validMoves = ["left", "right", "up", "down", "exit"]
+
+# userInput = ""
+# while True:
+#     userInput = checkValidity(validMoves, "Enter a valid move")
+#     if userInput == "exit":
+#         break
+#     moveTile(userInput)
+#     print()
+#     displayPuzzle()
--- python/test.py
+++ python/test.py
@@ -1,15 +1,3 @@
-def myFunction():
-    return 1, 2
+myList = ["1", "2", "3", "F"]
 
-myList = [
-    [0, 1, 2],
-    [3, 4, 5]
-]
-
-print(len(myList))
-print(len(myList[0]))
-
-for row in range(len(myList)):
-    for val in myList[row]:
-        print(val, end = " ")
-    print()
+print(int(myList[3], 16))
